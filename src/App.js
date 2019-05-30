a878dcd51728d5f7fce4908ab26104f0b7abdf78
Olivier JM <manolivier93@gmail.com>
2019-05-30 11:49:31 +0200

    update apps

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bongohive.co.zm"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to BongoHive, hoping you can join our dev team.
         </a>
       </header>
     </div>
